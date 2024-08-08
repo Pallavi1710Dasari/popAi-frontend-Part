4f5aab0ea4441d1dfc401a2be5d2205ac110c5b5
pallavi <dasaripallavi1710>
2024-08-08 14:55:27 +0530

    sidebar changes 3nd time

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,68 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import SideBar from "../SideBar";
 import { generateResponse, uploadImage, uploadPDF } from '../../services/apiServices';
-import { BsSendFill } from "react-icons/bs"
+import { BsSendFill } from "react-icons/bs";
+import { PiImageThin } from 'react-icons/pi';
+import { LuPlus } from 'react-icons/lu';
 import "./index.css"
  
 
-export const Home = () =>{
+export const Home = () => {
   const [input, setInput] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const [response, setResponse] = useState('');
-  const [imageFile, setImageFile] = useState(null);
-  const [pdfFile, setPdfFile] = useState(null);
+  const [isExpanded, setExpand] = useState(false);
+  const [imageSrc, setImageSrc] = useState(null);
 
-  const [isExpanded, setExpand] = useState(false)
+  const imageInputRef = useRef(null);
+  const pdfInputRef = useRef(null);
 
-  const onChangesidebar=()=>{
-      setExpand(!isExpanded)
-  }
+  const onChangesidebar = () => {
+    setExpand(!isExpanded);
+  };
 
   const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        const res = await generateResponse([{ role: 'user', content: [{ type: 'text', text: input }] }]);
-        setResponse(res.response);
-      } catch (error) {
-        console.error('Error generating response:', error);
-      }
-    };
-   
-    const handleImageUpload = async (e) => {
-      const file = e.target.files[0];
-      if (file) {
-        try {
-          const res = await uploadImage(file);
-          console.log('Image base64:', res.image_base64);
-        } catch (error) {
-          console.error('Error uploading image:', error);
-        }
-      }
-    };
-   
-    const handlePDFUpload = async (e) => {
-      const file = e.target.files[0];
-      if (file) {
-        try {
-          const res = await uploadPDF(file);
-          console.log('PDF images base64:', res.images_base64);
-        } catch (error) {
-          console.error('Error uploading PDF:', error);
-        }
-      }
-    };
+    e.preventDefault();
+    setIsSubmitted(!isSubmitted);
+    try {
+      const res = await generateResponse([{ role: 'user', content: [{ type: 'text', text: input }] }]);
+      setResponse(res.response);
+    } catch (error) {
+      console.error('Error generating response:', error);
+    }
+  };
 
-      console.log(isExpanded)
-      let sideClassName=isExpanded ? "sidebar-opened" : "sidebar-closed"
-      let containerClassName=isExpanded ? "body-container-with-full-sidebar" : "body-container-without-full-sidebar"
-   
-    return (
-      <div className='home-con'>
-          <div  className={sideClassName}>
-            <SideBar onChangesidebar={onChangesidebar} isExpanded = {isExpanded} />
-          </div>
-          <div className={containerClassName}>
-            <form onSubmit={handleSubmit}>
-             <div className='input-container' >        
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    setIsSubmitted(true)
+    if (file) {
+      try {
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setImageSrc(reader.result);
+        };
+        reader.readAsDataURL(file);
+
+        const res = await uploadImage(file);
+        console.log('Image base64:', res);
+        setResponse(JSON.stringify(res))
+
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    }
+  };
+
+  const handlePDFUpload = async (e) => {
+    const file = e.target.files[0];
+    // setInput(file)
+    setIsSubmitted(true)
+    if (file) {
+      try {
+        const res = await uploadPDF(file);
+        console.log('PDF images base64:', res.images_base64);
+        setResponse(JSON.stringify(res))
+      } catch (error) {
+        console.error('Error uploading PDF:', error);
+      }
+    }
+  };
+
+  const handleIconClick = (ref) => {
+    ref.current.click();
+  };
+
+  let sideClassName = isExpanded ? "sidebar-opened" : "sidebar-closed";
+  let containerClassName = isExpanded ? "body-container-with-full-sidebar" : "body-container-without-full-sidebar";
+
+  return (
+    <div className='home-con'>
+      <div className={sideClassName}>
+        <SideBar onChangesidebar={onChangesidebar} isExpanded={isExpanded} />
+      </div>
+      <div className={containerClassName}>
+        <form onSubmit={handleSubmit}>
+          <div className='input-container'>
             <textarea
               type="text"
               value={input}
@@ -71,20 +92,69 @@ export const Home = () =>{
               className='text-input'
             />
             <button className='input-button' type="submit">
-             <BsSendFill/>
+              <BsSendFill />
             </button>
-          </div>  
+          </div>
         </form>
-        <p>{input}</p>
-        <p>Response: {response}</p>
-   
-        <h2>Upload Image</h2>
-        <input type="file" accept="image/*" onChange={handleImageUpload} />
-   
-        <h2>Upload PDF</h2>
-        <input type="file" accept=".pdf" onChange={handlePDFUpload} />
+          {isSubmitted &&
+            <div className='response-container'>
+              <div>
+                <p>{input}</p>
+              </div>
+              {imageSrc && (
+            <div className='image-preview'>
+              <img src={imageSrc} alt="Selected" style={{ maxWidth: '100%', height: 'auto' }} />
+            </div>
+              )}
+              <div>
+                <p>{response}</p>
+              </div>
+              </div>
+          }
+        
+        {!isSubmitted &&
+          <div className='file-main-container'>
+            <div className='file-container'>
+              <div className=''>
+                <input
+                  type="file"
+                  accept="image/*"
+                  ref={imageInputRef}
+                  // style={{ display: 'none' }}
+                  onChange={handleImageUpload}
+                />
+              </div>
+              <div className=''>
+                <input
+                  type="file"
+                  accept=".pdf"
+                  ref={pdfInputRef}
+                  // style={{ display: 'none' }}
+                  onChange={handlePDFUpload}
+                />
+              </div>
+            </div>
+          </div>
+        }
+        <div className='icon-bg-con'>
+          <div className='icon-con'>
+            <button
+              className='icon-button'
+              onClick={() => handleIconClick(imageInputRef)}
+            >
+              <PiImageThin className='upload-file-icon' />
+            </button>
+          </div>
+          <div className='icon-con'>
+            <button
+              className='icon-button'
+              onClick={() => handleIconClick(pdfInputRef)}
+            >
+              <LuPlus className='upload-file-icon' />
+            </button>
+          </div>
         </div>
       </div>
-    );
-
+    </div>
+  );
 }
